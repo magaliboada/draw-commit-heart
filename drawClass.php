a6c20357baf8ab814a8a43c18d9fde9dcd8b14ec
Magalí Boada <boadamagali@gmail.com>
2020-06-03 11:35:04 +0200

    3 Jun 20200

--- drawClass.php
+++ drawClass.php
@@ -3,24 +3,24 @@ const FILENAME = 'fileToModify.txt';
 const COLOR_DEPTH = 1;
 
 $assignedDates = [
-    '"24 Feb 2020"',
-    '"25 Feb 2020"',
-    '"1 Mar 2020"',
-    '"4 Mar 2020"',
-    '"8 Mar 2020"',
-    '"12 Mar 2020"',
-    '"16 Mar 2020"',
-    '"20 Mar 2020"',
-    '"24 Mar 2020"',
-    '"28 Mar 2020"',
-    '"30 Mar 2020"',
-    '"3 Apr 2020"',
-    '"5 Apr 2020"',
-    '"9 Apr 2020"',
-    '"12 Apr 2020"',
-    '"15 Apr 2020"',
-    '"20 Apr 2020"',
-    '"21 Apr 2020"',
+    '"3 Jun 2020"',
+    '"4 Jun 2020"',
+//    '"1 Mar 2020"',
+//    '"4 Mar 2020"',
+//    '"8 Mar 2020"',
+//    '"12 Mar 2020"',
+//    '"16 Mar 2020"',
+//    '"20 Mar 2020"',
+//    '"24 Mar 2020"',
+//    '"28 Mar 2020"',
+//    '"30 Mar 2020"',
+//    '"3 Apr 2020"',
+//    '"5 Apr 2020"',
+//    '"9 Apr 2020"',
+//    '"12 Apr 2020"',
+//    '"15 Apr 2020"',
+//    '"20 Apr 2020"',
+//    '"21 Apr 2020"',
 ];
 
 foreach ($assignedDates as $item) {
@@ -29,7 +29,8 @@ foreach ($assignedDates as $item) {
         shell_exec("git add .");
         shell_exec("git commit -m $item$i");
         shell_exec("git commit --amend --no-edit --date $item$i");
-        shell_exec("git pull --rebase");
-        shell_exec("git push origin master");
     }
 }
+
+shell_exec("git pull --rebase");
+shell_exec("git push origin master");
